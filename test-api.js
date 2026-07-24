e8340f4ed315337f23ff7ab3f3454c4b806a63d7
sikanda <simosdatangsel@gmail.com>
2026-07-24 13:51:47 +0700

    chore: cleanup project and apply production security hardening

--- test-api.js
+++
@@ -1,2 +0,0 @@
-const assert = require('assert');
-// Just checking if email_confirm works in Supabase Admin API
--- test-auth.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-function credentialPassword_(nip, rawPassword, pepper) {
-  const signature = crypto.createHmac("sha256", pepper)
-    .update(String(rawPassword) + '\n' + String(nip))
-    .digest();
-  return signature.toString('base64url').replace(/=+$/g, '');
-}
-console.log("Derived:", credentialPassword_('111111111111111111', 'IceTo152041gmail', 'this_is_a_test_pepper_1234567890'));
